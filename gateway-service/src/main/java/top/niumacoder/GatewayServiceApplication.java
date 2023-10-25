d4b614e895519037ad5db62dd293f0f8a9c11640
hape <9092148+hapeman@user.noreply.gitee.com>
2023-10-25 20:49:48 +0800

    fix：修改启动类

---
+++ gateway-service/src/main/java/top/niumacoder/GatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package top.niumacoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+}
--- user-service/src/main/java/top/niumacoder/userservice/UserServiceApplication.java
+++ user-service/src/main/java/top/niumacoder/userservice/UserServiceApplication.java
@@ -1,12 +1,13 @@
 package top.niumacoder.userservice;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("top.niumacoder.userservice.dao.mapper")
-public class Main {
+public class UserServiceApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 }

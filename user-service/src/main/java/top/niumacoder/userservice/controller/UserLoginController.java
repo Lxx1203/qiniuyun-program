fc9a6a2ac8e3e5b37ea58045b73773c50c79bd3e
hape <9092148+hapeman@user.noreply.gitee.com>
2023-10-26 15:24:28 +0800

    feature：用户登录控制层

--- user-service/src/main/java/top/niumacoder/userservice/controller/UserLoginController.java
+++ user-service/src/main/java/top/niumacoder/userservice/controller/UserLoginController.java
@@ -1,12 +1,14 @@
 package top.niumacoder.userservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import top.niumacoder.frameworks.convention.result.Result;
 import top.niumacoder.frameworks.web.Results;
+import top.niumacoder.userservice.dto.req.UserLoginReqDTO;
 import top.niumacoder.userservice.dto.req.UserMessageReqDTO;
+import top.niumacoder.userservice.dto.req.UserRegisterReqDTO;
+import top.niumacoder.userservice.dto.resp.UserLoginRespDTO;
+import top.niumacoder.userservice.dto.resp.UserRegisterRespDTO;
 import top.niumacoder.userservice.service.UserLoginService;
 
 /**
@@ -21,5 +23,34 @@ public class UserLoginController {
     public Result<String> sendMsg(@RequestBody UserMessageReqDTO requestParam) {
         userLoginService.sendMsg(requestParam);
         return Results.success("验证码已发送");
+    }
+
+    @PostMapping("/user-service/api/loginByPasswrod")
+    public Result<UserLoginRespDTO> loginByPassword(@RequestBody UserLoginReqDTO requestParam) {
+        UserLoginRespDTO result = userLoginService.loginByPassword(requestParam);
+        return Results.success(result);
+    }
+
+    @PostMapping("/user-service/api/loginByCode")
+    public Result<UserLoginRespDTO> loginByCode(@RequestBody UserLoginReqDTO requestParam) {
+        UserLoginRespDTO result = userLoginService.loginByCode(requestParam);
+        return Results.success(result);
+    }
+
+    @PostMapping("/user-service/api/register")
+    public Result<UserRegisterRespDTO> register(@RequestBody UserRegisterReqDTO requestParam) {
+        UserRegisterRespDTO result = userLoginService.register(requestParam);
+        return Results.success(result);
+    }
+
+    @GetMapping("/user-service/api/check-login")
+    public Result<UserLoginRespDTO> checkLogin(@RequestParam("accessToken") String accessToken) {
+        UserLoginRespDTO result = userLoginService.checkLogin(accessToken);
+        return Results.success(result);
+    }
+
+    public Result<Void> logout(@RequestParam(required = false) String accessToken) {
+        userLoginService.logout(accessToken);
+        return Results.success();
     }
 }
